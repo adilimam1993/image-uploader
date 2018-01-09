c79aaf831dce253e69514b912088687e7554844e
Imam <simam@CVOptical.local>
2018-01-09 16:38:10 -0500

    Implemented Upload s3

--- src/index.js
+++ src/index.js
@@ -1,66 +1,51 @@
 import express from 'express';
 import corsPrefetch from 'cors-prefetch-middleware';
 import imagesUpload from 'images-upload-middleware';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use('/static', express.static('./static'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(corsPrefetch);
 
 app.post('/multiple', imagesUpload(
     './static/multipleFiles',
-    'http://localhost:9090/static/multipleFiles',
-    true
+    'http://localhost:9090/static/multipleFiles'
 ));
 
 
-app.post('/uploadToS3', function(req, res){
+app.post('/uploadToS3', function(req, res) {
 
-    var s3 = require('s3');
-    // create s3 client
-    var client = s3.createClient({
-        maxAsyncS3: 20,     // this is the default
-        s3RetryCount: 3,    // this is the default
-        s3RetryDelay: 1000, // this is the default
-        multipartUploadThreshold: 20971520, // this is the default (20 MB)
-        multipartUploadSize: 15728640, // this is the default (15 MB)
-        s3Options: {
-            accessKeyId: "your s3 key",
-            secretAccessKey: "your s3 secret",
-            region: "your region",
-            // endpoint: 's3.yourdomain.com',
-            // sslEnabled: false
-            // any other options are passed to new AWS.S3()
-            // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
-        },
-    });
+    var path = require('path');
+    var image = path.basename(req.body.image);
+    var AWS = require('aws-sdk');
 
-    var params = {
-        localFile: "some/local/file",
+    AWS.config.update({region: 'us-east-1', accessKeyId:'AKIAIZHU5EC7F2FIBNVA', secretAccessKey:'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'});
 
-        s3Params: {
-            Bucket: "s3 bucket name",
-            Key: "some/remote/file",
-            // other options supported by putObject, except Body and ContentLength.
-            // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
-        },
-    };
+        var s3 = new AWS.S3({apiVersion: '2006-03-01'});
+        var uploadParams = {Bucket: 'lost-and-found-bucket', Key: '', Body: ''};
+        var file = "./static/multipleFiles/"+image;
 
-    var uploader = client.uploadFile(params);
-    uploader.on('error', function(err) {
-        console.error("unable to upload:", err.stack);
-    });
-    uploader.on('progress', function() {
-        console.log("progress", uploader.progressMd5Amount,
-            uploader.progressAmount, uploader.progressTotal);
-    });
-    uploader.on('end', function(){
-        console.log("done uploading");
-    });
-}
+        var fs = require('fs');
+        var fileStream = fs.createReadStream(file);
+        fileStream.on('error', function(err) {
+            console.log('File Error', err);
+        });
+        uploadParams.Body = fileStream;
+        uploadParams.Key = path.basename(file);
+
+        s3.upload (uploadParams, function (err, data) {
+            if (err) {
+                console.log("Error", err);
+            } if (data) {
+                console.log("Upload Success", data.Location);
+            }
+        });
+    }
 );
-
 
 
 app.post('/notmultiple', imagesUpload(
@@ -71,3 +56,5 @@ app.post('/notmultiple', imagesUpload(
 app.listen(9090, () => {
     console.log('Listen: 9090');
 });
+
+
